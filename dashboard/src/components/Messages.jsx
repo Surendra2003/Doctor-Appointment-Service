1c57f861447b6d7c5701d1712368cf30b5889ca7
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-11-01 09:57:51 +0530

    Update Messages.jsx
    Backend URL Update

--- dashboard/src/components/Messages.jsx
+++ dashboard/src/components/Messages.jsx
@@ -11,7 +11,8 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/message/getall",
+          "https://doctor-appointment-service-backend.onrender.com/api/v1/message/getall",
+          //"http://localhost:4000/api/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
