8ff59395abdd59e7e8d30b8e9a3c95c1f2525080
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-11-01 09:51:27 +0530

    Update AddNewAdmin.jsx

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,8 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/admin/addnew",
+          "https://doctor-appointment-service-backend.onrender.com/api/v1/user/admin/addnew",
+          // "http://localhost:4000/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
