27259addd6509278d0ab343cebe91aaae8f7e261
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-11-01 08:16:39 +0530

    Update App.jsx
    Backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,8 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/patient/me",
+          "https://doctor-appointment-service-backend.onrender.com/api/v1/user/patient/me",
+           // "http://localhost:4000/api/v1/user/patient/me",
           {
             withCredentials: true,
           }
