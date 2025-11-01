ee1df213ec7ed3f3693602b87de149e7e719f391
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-11-01 08:15:27 +0530

    Update Register.jsx
    Backed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -23,7 +23,8 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          "https://doctor-appointment-service-backend.onrender.com/api/v1/user/patient/register",
+           // "http://localhost:4000/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
