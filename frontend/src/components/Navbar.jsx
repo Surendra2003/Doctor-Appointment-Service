267ddec5fb4e5a06cc43c79176d1ee2567391ff0
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2025-11-01 08:12:50 +0530

    Update Navbar.jsx
    Backend Connection

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,9 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("http://localhost:4000/api/v1/user/patient/logout", {
+      .get("https://doctor-appointment-service-backend.onrender.com/api/v1/user/patient/logout",
+           // .get("http://localhost:4000/api/v1/user/patient/logout",
+           {
         withCredentials: true,
       })
       .then((res) => {
